a52a79ad6a76a1d718df04af6fe7b52dd6eb8e63
Marcus Vinicius <marcusvbs2018@gmail.com>
2024-09-27 16:28:46 -0300

    SellerService and basic FindAll

---
+++ SalesWeb/Services/SellersService.cs
@@ -0,0 +1,22 @@
+﻿using SalesWeb.Data;
+using SalesWeb.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWeb.Services
+{
+    public class SellersService
+    {
+        private readonly SalesWebContext _context;
+
+        public SellersService(SalesWebContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- SalesWeb/Startup.cs
+++ SalesWeb/Startup.cs
@@ -8,6 +8,7 @@ using SalesWeb.Data;
 using System.Security.Cryptography;
 using Microsoft.CodeAnalysis.Scripting.Hosting;
 using System;
+using SalesWeb.Services;
 
 namespace SalesWeb
 {
@@ -31,6 +32,7 @@ namespace SalesWeb
             );
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellersService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
